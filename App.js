0c7f13e16c99c0156b22935c992e6f332fff5a0f
WheatleyTheCore <alex.colwell@icloud.com>
2021-09-05 18:13:59 -0400

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  const [data, setData] = useState("");
+  const [storedData, setStoredData] = useState("");
+
+  const saveData = async (data) => {
+    try {
+      await AsyncStorage.setItem("test", data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const loadData = async (setData) => {
+    try {
+      const val = await AsyncStorage.getItem("test");
+      setStoredData(val);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          width: "80%",
+        }}
+        onChangeText={(text) => setData(text)}
+        value={data}
+        onSubmitEditing={() => saveData(data)}
+      />
+      <Text>Data in storage: {storedData}</Text>
     </View>
   );
 }
@@ -14,8 +49,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
