d7b806e1aa35377f8b77bee18d7b6abb0c7084ad
WheatleyTheCore <alex.colwell@icloud.com>
2021-12-29 12:46:45 -0500

    stack navigation verified working

--- second-try/App.tsx
+++ second-try/App.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
+      <TouchableOpacity onPress={() => {
+        navigation.navigate("Settings")
+      }}>
+        <Text>Settings page</Text>
+      </TouchableOpacity>
     </View>
   );
 }
