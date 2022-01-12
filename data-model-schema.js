2619a370b9b3e255ff3d790c2be26a9e5dac159f
WheatleyTheCore <alex.colwell@icloud.com>
2022-01-12 15:19:42 -0500

    added data-model-schema

---
+++ data-model-schema.js
@@ -0,0 +1,9 @@
+collection: {
+    skeleton: {
+        //dehydrated version of an entry.
+    }
+    notes: // short description of  model any other notes that the user might need
+    entries: [
+     //array of entries 
+    ]
+}
