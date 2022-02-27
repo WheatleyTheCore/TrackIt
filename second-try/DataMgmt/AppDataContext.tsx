001472d7560f30337ead6da0682f9f2b2143c295
WheatleyTheCore <alex.colwell@icloud.com>
2022-02-27 11:29:07 -0500

    added some docs and updated all db interaction functions

--- second-try/DataMgmt/AppDataContext.tsx
+++ second-try/DataMgmt/AppDataContext.tsx
@@ -40,11 +40,13 @@ const defaultCollection: CollectionInterface = {
 export const AppDataContext = createContext<AppContextInterface | null>(null)
 
 export const AppDataContextProvider = (props: any): ReactElement => {
-    const [collectionNames, setCollectionNames] = useState<string[]>(['asdf'])
+    const [collectionNames, setCollectionNames] = useState<string[]>([])
     const [currentCollection, setCurrentCollection] = useState<Collection>(props.defaultCollection)
     const [collections, setCollections] = useState<Collection[]>([]);
 
     const database = SQLite.openDatabase('db.db');
+
+    console.log(collectionNames)
 
     //create table for collections (TODO abstract into setup function)
     database.transaction(tx => {
@@ -52,8 +54,18 @@ export const AppDataContextProvider = (props: any): ReactElement => {
         tx.executeSql('create table if not exists collections (id integer primary key autoincrement, name text unique, entry_schema text, entries text)')
     })
 
-    const loadAllCollections = () => {
-        let collections = [];
+    const getAllCollectionNames = () => {
+        let collectionNames: string[] = [];
+        database.transaction(tx => {
+            tx.executeSql("select name from collections", [], (_, data) => {
+                data.rows._array.forEach(item => {
+                    collectionNames.push(item['name'])
+                })
+                setCollectionNames(collectionNames)
+            }, (_, err)=>{
+                console.log(err)
+            })
+        })
     }
 
     const createCollection = (collectionToSave: Collection) => {
@@ -70,10 +82,11 @@ export const AppDataContextProvider = (props: any): ReactElement => {
         })
     }
 
+    //updates name and entrySchema
     const updateCollection = (collectionToSave: Collection, name: string) => {
         let updatedName = collectionToSave.name;
         let updatedEntrySchema = JSON.stringify(collectionToSave.entrySchema);
-        let updatedEntries = JSON.stringify([]);
+        let updatedEntries = JSON.stringify(collectionToSave.entries);
 
         database.transaction(tx => {
             tx.executeSql("update collections set name = ?, entry_schema = ?, entries = ?  where name = ?", [updatedName, updatedEntrySchema, updatedEntries, name], () => {
@@ -94,22 +107,47 @@ export const AppDataContextProvider = (props: any): ReactElement => {
         })
     }
 
-    const removeCollection = async (collectionName: string) => {
-        deleteCollectionFromDisk(collectionName)
-        let updatedNameList = collectionNames.filter(name => name != collectionName)
-        setCollectionNames(updatedNameList)
-    }
-
-    const saveEntries = (entries: Entry[]) => {
-        setCurrentCollection({
-            ...currentCollection,
-            entries: entries
+    const addEntry = (entry: Entry, collectionName: string) => {
+        database.transaction(tx => {
+            tx.executeSql("select entries from collections where name = ?", [collectionName], (t, data) => {
+                let entryObject = data.rows._array[0]
+                let entryArray = JSON.parse(entryObject.entries)
+                entryArray.push(entry)
+                tx.executeSql('update collections set entries = ? where name = ?', [JSON.stringify(entryArray), collectionName])
+            }, (_, err)=>{
+                console.log(err)
+            })
         })
     }
 
-    const removeEntry = (entryID: string) => {
-        let updatedEntries = currentCollection.entries.filter(entry => entry.ID != entryID)
-        saveEntries(updatedEntries)
+    const updateEntry = (entry: Entry, index: number, collectionName: string) => {
+        database.transaction(tx => {
+            tx.executeSql("select entries from collections where name = ?", [collectionName], (t, data) => {
+                let entryObject = data.rows._array[0]
+                let entryArray = JSON.parse(entryObject.entries)
+                if (index < entryArray.length) {
+                    entryArray[index] = entry
+                }
+                tx.executeSql('update collections set entries = ? where name = ?', [JSON.stringify(entryArray), collectionName])
+            }, (_, err)=>{
+                console.log(err)
+            })
+        })
+    }
+
+    
+
+    const deleteEntry = (index: number, collectionName: string) => {
+        database.transaction(tx => {
+            tx.executeSql("select entries from collections where name = ?", [collectionName], (t, data) => {
+                let entryObject = data.rows._array[0]
+                let entryArray = JSON.parse(entryObject.entries)
+                entryArray.splice(index, 1)
+                tx.executeSql('update collections set entries = ? where name = ?', [JSON.stringify(entryArray), collectionName])
+            }, (_, err)=>{
+                console.log(err)
+            })
+        })
     }
 
     const collectionFactory = () => {
@@ -156,13 +194,37 @@ export const AppDataContextProvider = (props: any): ReactElement => {
                 deleteCollection('test collection');
             }} title="delete row" />
             <Button onPress={() => {
-                database.transaction(tx => {
-                    tx.executeSql("select * from collections", [], (_, data) => {
-                        console.log("-------------data from db------------")
-                        console.log(data)
-                    })
-                })
-            }} title="read some data" />
+                let collection = {
+                    name: 'test collection',
+                    entrySchema: {
+                        time: 'number',
+                        place: 'text'
+                    },
+                    entries: []
+                }
+                createCollection(collection);
+                let collection2 = {
+                    name: 'test collection 2',
+                    entrySchema: {
+                        time: 'number',
+                        place: 'text'
+                    },
+                    entries: []
+                }
+                createCollection(collection2);
+                let collection3 = {
+                    name: 'test collection 3',
+                    entrySchema: {
+                        time: 'number',
+                        place: 'text'
+                    },
+                    entries: []
+                }
+                createCollection(collection3);
+
+                getAllCollectionNames()
+                console.log(collectionNames)
+            }} title="test the function!" />
         </AppDataContext.Provider>
     )
 }
--- second-try/screens/Home.tsx
+++ second-try/screens/Home.tsx
@@ -10,37 +10,37 @@ import { AppDataContext } from '../DataMgmt/AppDataContext'
 
 export default ({navigation}:any) => {
 
-    const context = useContext(AppDataContext)
-    const [didLoadCollectionNames, setDidLoadCollectionNames] = useState(false)
+    // const context = useContext(AppDataContext)
+    // const [didLoadCollectionNames, setDidLoadCollectionNames] = useState(false)
 
 
-    const clickHandler = (destination: string, params?: object): void => {
-        navigation.navigate(destination, params ? params : null)
-    }
+    // const clickHandler = (destination: string, params?: object): void => {
+    //     navigation.navigate(destination, params ? params : null)
+    // }
 
-    const createNewCollectionHandler = (): void => {
-        context.currentCollection = context.collectionFactory();
-        navigation.navigate("AddCollection")
-    }
+    // const createNewCollectionHandler = (): void => {
+    //     context.currentCollection = context.collectionFactory();
+    //     navigation.navigate("AddCollection")
+    // }
     
 
-    useEffect(() => {
-        if (!didLoadCollectionNames) {
-            loadAllCollectionNames().then(collectionNames => {
-                context?.setCollectionNames(collectionNames)
-                context?.saveCollection({entrySchema: {},
-                    notes:"",
-                    entries: []}, 'testing')
-            })
-            setDidLoadCollectionNames(true)
-        }
-    }, [didLoadCollectionNames])
+    // useEffect(() => {
+    //     if (!didLoadCollectionNames) {
+    //         loadAllCollectionNames().then(collectionNames => {
+    //             context?.setCollectionNames(collectionNames)
+    //             context?.saveCollection({entrySchema: {},
+    //                 notes:"",
+    //                 entries: []}, 'testing')
+    //         })
+    //         setDidLoadCollectionNames(true)
+    //     }
+    // }, [didLoadCollectionNames])
 
     //TODO the keys for stuff are now GUIDs, we'll need to load all the collections into memory to get their names. 
     return (
         <SafeAreaView>
-            <List listData={context?.collectionNames} clickHandler={clickHandler} deleteHandler={context?.removeCollection} />
-            <Button title="create new collection" onPress={() => createNewCollectionHandler()} />
+            {/* <List listData={context?.collectionNames} clickHandler={clickHandler} deleteHandler={context?.removeCollection} />
+            <Button title="create new collection" onPress={() => createNewCollectionHandler()} /> */}
         </SafeAreaView>
     )
 }
